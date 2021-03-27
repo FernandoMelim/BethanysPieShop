970bee7d743ad39f6db659a3ec5367f1dd55dc75
Fernando Hottum <Fernando.Hottum@thomsonreuters.com>
2021-03-26 23:26:20 -0300

    Asp.NET Core MVC course

---
+++ BethanysPieShop/Models/CategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext db;
+
+        public CategoryRepository(AppDbContext appDbContext)
+        {
+            db = appDbContext;
+        }
+
+        public IEnumerable<Category> AllCategories => db.Categories;
+
+    }
+}
---
+++ BethanysPieShop/Models/PieRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BethanysPieShop.Models
+{
+    public class PieRepository: IPieRepository
+    {
+        private readonly AppDbContext db;
+
+        public PieRepository(AppDbContext appDbContext)
+        {
+            db = appDbContext;
+        }
+
+        public IEnumerable<Pie> AllPies
+        {
+            get
+            {
+                return db.Pies.Include(c => c.Category);
+            }
+        }
+
+        public IEnumerable<Pie> PiesOfTheWeek
+        {
+            get
+            {
+                return db.Pies.Include(c => c.Category).Where(p => p.IsPieOfTheWeek);
+            }
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            return db.Pies.FirstOrDefault(p => p.PieId == pieId);
+        }
+    }
+}
